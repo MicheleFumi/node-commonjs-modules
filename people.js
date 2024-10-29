4a2d713d4d8b4b53202cd8f054449662eb07e18e
MicheleFumi <michele.fumi96@gmail.com>
2024-10-29 16:53:31 +0100

    change files location

---
+++ people.js
@@ -0,0 +1,13 @@
+
+const names = require('./names.js')
+const hobbies = require('./hobbies.js')
+
+const fullName =fullName("Marco","Rossi")
+const hobbie = hobbies("basket","TCG","gaming")
+
+console.log(fullName);
+
+function peopleHobbies() {
+ 
+}
+
